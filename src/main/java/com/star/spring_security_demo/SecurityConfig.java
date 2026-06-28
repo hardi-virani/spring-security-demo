c6ed7cf2021c67097da1c25c6205c1ccb440d516
Hardi <hardivirani2003@gmail.com>
2026-06-28 15:57:02 -0400

    vid : 147 - Disabling CSRF token

--- src/main/java/com/star/spring_security_demo/SecurityConfig.java
+++ src/main/java/com/star/spring_security_demo/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.star.spring_security_demo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
@@ -11,6 +13,16 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        just because we have written this securityfilterchai method the spring security defualt setting (which is asking username, password) goes away, because we are doing configuration by ourselves.
+
+        http.csrf(customizer -> customizer.disable()); // this disables the csrf
+
+        http.authorizeHttpRequests(request -> request.anyRequest().authenticated()); //this means we are enableing askin username password for any request.
+//        http.formLogin(Customizer.withDefaults()); //we dont need when you have stateless
+        http.httpBasic(Customizer.withDefaults());
+
+        //making stateless, because it is making stateful by default, basically we want to get new session
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
 
