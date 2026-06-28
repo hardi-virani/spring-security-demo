dbc4bb30f331c5097c6db97b9629a9c505daae0c
Hardi <hardivirani2003@gmail.com>
2026-06-28 09:49:46 -0400

    vid : 139 - Session ID

--- src/main/java/com/star/spring_security_demo/HelloController.java
+++ src/main/java/com/star/spring_security_demo/HelloController.java
@@ -1,5 +1,6 @@
 package com.star.spring_security_demo;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("hello")
-    public String greet() {
-        return "Hello World";
+    public String greet(HttpServletRequest request) {
+        System.out.println("This is the session ID : " + request.getSession().getId());
+        return "Hello World " + request.getSession().getId();
+    }
+
+    @GetMapping("/about")
+    public String about(HttpServletRequest request) {
+        return "star " + request.getSession().getId();
     }
 }
