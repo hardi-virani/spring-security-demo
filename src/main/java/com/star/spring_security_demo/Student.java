515461164c5d01de48de0ecf08251d6a9ca1a236
Hardi <hardivirani2003@gmail.com>
2026-06-28 10:50:47 -0400

    vid : 143 - Error without CSRF token

---
+++ src/main/java/com/star/spring_security_demo/Student.java
@@ -0,0 +1,18 @@
+package com.star.spring_security_demo;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+
+    private int id;
+    private String name;
+    private String tech;
+
+
+}
---
+++ src/main/java/com/star/spring_security_demo/StudentController.java
@@ -0,0 +1,32 @@
+package com.star.spring_security_demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class StudentController {
+
+    List<Student> students = new ArrayList<>(List.of(
+            new Student(1, "hardi", "Java"),
+            new Student(2, "mann", "MLOps")
+
+    ));
+
+    @GetMapping("students")
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    @PostMapping("/students")
+    public void addStudent(@RequestBody Student student) {
+
+        students.add(student);
+
+    }
+
+}
