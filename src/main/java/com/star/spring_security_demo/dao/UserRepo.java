795e553217ec24fcd05a2e6f20ca15203d00cca9
Hardi <hardivirani2003@gmail.com>
2026-06-29 21:20:12 -0400

    vid : 158 - User Registration

--- src/main/java/com/star/spring_security_demo/dao/UserRepo.java
+++ src/main/java/com/star/spring_security_demo/dao/UserRepo.java
@@ -2,7 +2,9 @@ package com.star.spring_security_demo.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.star.spring_security_demo.model.User;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepo extends JpaRepository <User, Integer> {
     //8. Now using this repo which has a method called findUserByUsername.
 //    9. And we are using JPA so we don't have to write the SQL queries. (to go MyUserDetialsService)
